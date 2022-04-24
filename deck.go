5549944be6363e85669decdf67e7ca3b38e78454
Abanoub Fathy <abanoub.fathy.me@gmail.com>
2022-04-24 13:40:43 +0200

    Initial Commit

---
+++ deck.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+)
+
+// define a deck type which is []string
+type deck []string
+
+// method to create a new deck
+func CreateNewDeck() deck {
+	cards := deck{}
+
+	// suits and values
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	// loop
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			card := value + " of " + suit
+			cards = append(cards, card)
+		}
+	}
+
+	return cards
+}
+
+// method to print all the cards in the deck
+func (d deck) PrintAllCards() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+// method to deal
+// This method returns the hands cards and the remaining cards
+func (d deck) Deal(handNum int) (deck, deck) {
+	return d[:handNum], d[handNum:]
+}
+
+// method to convert a deck to string
+func (d deck) ConvertToString() string {
+	return strings.Join(d, ",")
+}
+
+// method to save Deck to a file
+func (d deck) SaveToFile(fileName string) {
+	err := ioutil.WriteFile(fileName, []byte(d.ConvertToString()), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// method to create a new deck from file
+func CreateDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// convert the bs to string
+	content := string(bs)
+
+	// convert content to []string
+	cards := strings.Split(content, ",")
+
+	// return deck
+	return deck(cards)
+}
